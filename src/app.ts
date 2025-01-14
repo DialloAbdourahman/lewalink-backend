f3e79766efd63845de7d3a84403901b46f0899dd
DialloAbdourahman <dialliabdourahman78@gmail.com>
2025-01-14 12:46:17 +0100

    fixed cors errors

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,14 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:5173"], // Allow specific origins
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  credentials: true, // Allow credentials (cookies, etc.)
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
